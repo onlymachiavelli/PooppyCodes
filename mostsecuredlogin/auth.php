75d770c2c03a91a59cbb1951aee7596b43e84a60
onlymachiavelli <tmakaveli643@gmail.com>
2021-11-10 00:14:17 +0100

    meme

--- mostsecuredlogin/auth.php
+++ mostsecuredlogin/auth.php
@@ -12,8 +12,38 @@ $conn = new mysqli(
     "bored"
 );
 
+function Auth($user, $pass){
+    global $conn;
+    $action = "SELECT * FROM users";
+        $res = mysqli_query($conn, $action);
+        while ($row = $res -> fetch_assoc()){
+            if($row['email'] == $user && $row['password'] == $pass) {
+                return TRUE;
+            }
+        }
+        return FALSE;
+}
+
+
+$ver = FALSE;
 if($conn){
-    print("cv");
+    $sql = "SELECT * from users";
+    if(Auth($_POST["username"], $_POST["password"]) ) {
+        print("COOL URE IN");
+    }
+    else {
+        
+        $res = mysqli_query($conn, $sql);
+        while($row = $res -> fetch_assoc()){
+            if($row["password"] == $_POST["password"] && $row["email"] != $_POST["username"]){
+                print("Error login but this password's belongs to " . $row["email"]) ;
+                $ver = TRUE;
+                break;
+            }
+        }
+
+        if (!$ver) print("Error Logining in");
+    }
    
     
 }
