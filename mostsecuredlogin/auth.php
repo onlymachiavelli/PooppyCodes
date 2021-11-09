dd91727129ce653c02e786f584943415f32d87ff
onlymachiavelli <tmakaveli643@gmail.com>
2021-11-09 23:59:11 +0100

    ui

--- mostsecuredlogin/auth.php
+++ mostsecuredlogin/auth.php
@@ -1,3 +1,24 @@
-<?ph
+<?php
 
-p?>
+
+$hostname ="localhost";
+$username = "root";
+$password = "";
+
+$conn = new mysqli(
+    $hostname,
+    $username,
+    $password,
+    "bored"
+);
+
+if($conn){
+    print("cv");
+   
+    
+}
+
+
+else die("fcking db error");
+
+?>
